22fd2f724970b38d9eadf4adb5c4eaabea0d9b11
Che <che-dev@outlook.com>
2023-10-27 19:56:30 +1100

    adjust article styles for desktop

--- app/components/articleComponents.js
+++ app/components/articleComponents.js
@@ -1,6 +1,6 @@
 function ArticleHeading(props) {
     return (
-        <div className="flex items-center justify-center mb-8">
+        <div className="flex items-center justify-center mb-8 md:mb-12">
             <h1 className="inline-block text-2xl font-semibold text-primary-100 text-center pb-2 border-b-2 border-b-primary-120">{props.content}</h1>
         </div>
     )
@@ -8,7 +8,7 @@ function ArticleHeading(props) {
 
 function ArticleSectionHeading(props) {
     return (
-        <div className="flex items-center justify-center mb-4">
+        <div className="flex items-center justify-center mb-4 md:mb-8">
             <h2 className="text-base font-medium text-neutrals-100 text-center">{props.content}</h2>
         </div>
     )
@@ -16,7 +16,7 @@ function ArticleSectionHeading(props) {
 
 function ArticleText(props) {
     return (
-        <div className="flex items-start justify-normal mb-2">
+        <div className="flex items-start justify-normal mb-2 md:px-40">
             <p className="text-sm text-neutrals-80">{props.content}</p>
         </div>
     )
